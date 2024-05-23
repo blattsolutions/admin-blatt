74adf58e947a2821fc07db643d8cac8091f657ab
MinWanggg <minwanggg@Mac-mini-cua-MinWanggg.local>
2024-05-23 15:16:12 +0700

    update login

--- src/pages/auth/jwt/login.jsx
+++ src/pages/auth/jwt/login.jsx
@@ -8,7 +8,7 @@ export default function LoginPage() {
   return (
     <>
       <Helmet>
-        <title> Jwt: Login</title>
+        <title> BLATTBILD ADMIN: Login</title>
       </Helmet>
 
       <JwtLoginView />
